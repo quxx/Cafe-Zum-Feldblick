4b4c8dc642bb8ecbf7fd7faca5156b456ba59a02
Thomas Claudi <thomas.claudi@soprasteria.com>
2024-11-24 16:47:04 +0100

    Favicon

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<?php wp_head(); ?>
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/img/favIcon.png">
+ 
 </head>
 
 <body <?php body_class(); ?>>

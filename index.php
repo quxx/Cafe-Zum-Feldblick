776834de011257c15f4101a810b950acd9f77fd2
Verena Lehmann <verena.lehmann@soprasteria.com>
2024-11-29 15:59:49 +0100

    Riss-svg und Speisekarte-Button

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ get_header();
 
 $page_id = get_option('page_for_posts');
 ?>
-<section class="position-relative overflow-hidden">
+<section class="position-relative overflow-hidden riss-container">
 	<div class="d-flex min-vh-100" lc-helper="video-bg">
 		<video style="object-fit: cover; object-position: 50% 50%;" class="w-100 min-vh-100 parallax-video" autoplay preload muted loop playsinline>
 			<!-- adjust object-position to tweak cropping on mobile -->
@@ -24,6 +24,7 @@ $page_id = get_option('page_for_posts');
 			</div>
 		</div>
 	</div>
+	<div class="riss-svg bottom"></div>
 </section>
 <section class="container-fluid block block-padding ps-5 pe-5">
 	<div editable="rich" class="text-center oeffnungzeiten">
@@ -73,7 +74,8 @@ $page_id = get_option('page_for_posts');
 	</div>
 </section>
 
-<section class="container-fluid pe-0 ps-0 ms-0 me-0 block">
+<section class="container-fluid pe-0 ps-0 ms-0 me-0 block riss-container">
+	<div class="riss-svg"></div>
 	<div class="row pe-0 ps-0 me-0">
 		<div class=" col-lg-4 col-md-4 col-sm-4 col-xs-12 imagetiles pe-0 ps-0">
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/fahrradcafe-1.jpeg" height="auto" alt="Das Fahrradcafe von vorne">
@@ -85,6 +87,7 @@ $page_id = get_option('page_for_posts');
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/fahrradcafe-garten.jpg" height="auto" alt="Gartenansicht vom Fahrradcafe">
 		</div>
 	</div>
+	<div class="riss-svg bottom"></div>
 </section>
 <section class="container-fluid pe-0 ps-0 ms-0 me-0 block block-padding">
 	<div class="text-center">
@@ -125,7 +128,8 @@ $page_id = get_option('page_for_posts');
 		</div>
 	</div>
 </section>
-<section class="container-fluid pe-0 ps-0 ms-0 me-0 block">
+<section class="container-fluid pe-0 ps-0 ms-0 me-0 block riss-container">
+	<div class="riss-svg"></div>
 	<div class="row pe-0 ps-0 me-0">
 		<div class=" col-lg-4 col-md-4 col-sm-4 col-xs-12 imagetiles pe-0 ps-0">
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/fahrradcafe-getraenk.jpeg" height="auto" alt="Das Fahrradcafe von vorne">
@@ -137,6 +141,7 @@ $page_id = get_option('page_for_posts');
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/fahrradcafe-getraenk2.jpeg" height="auto" alt="Gartenansicht vom Fahrradcafe">
 		</div>
 	</div>
+	<div class="riss-svg bottom"></div>
 </section>
 <section class="container-fluid block block-padding ps-5 pe-5">
 	<div editable="rich" class="text-center oeffnungzeiten">
@@ -193,6 +198,9 @@ $page_id = get_option('page_for_posts');
 				
 			?>
 		</div>
+		<div class="row justify-content-center">
+		<a href="" class="btn btn-outline-primary btn-block col-lg-4 col-md-4 col-sm-6 col-xs-6">Zur Speisekarte</a>
+		</div>
 		<!-- <div class="row pe-0 ps-0 me-0">
 			<div class=" col-lg-6 col-md-6 col-sm-12 col-xs-12">
 				<div class="mkd-pli-content-holder d-flex flex-column align-items-start">
@@ -229,7 +237,8 @@ $page_id = get_option('page_for_posts');
 		</div> -->
 	</div>
 </section>
-<section class="container-fluid pe-0 ps-0 ms-0 me-0 block">
+<section class="container-fluid pe-0 ps-0 ms-0 me-0 block riss-container">
+	<div class="riss-svg"></div>
 	<div class="row pe-0 ps-0 me-0">
 		<div class=" col-lg-4 col-md-4 col-sm-4 col-xs-12 imagetiles pe-0 ps-0">
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/fahrradcafe-fahrrad.jpg" height="auto" alt="Das Fahrradcafe von vorne">
@@ -241,6 +250,7 @@ $page_id = get_option('page_for_posts');
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/fahrradcafe-ausblick.jpg" height="auto" alt="Gartenansicht vom Fahrradcafe">
 		</div>
 	</div>
+	<div class="riss-svg bottom"></div>
 </section>
 <section class="container-fluid block block-padding kontakt">
 	<div class="text-center">
